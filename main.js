b97bc4517f955c8f245d1033030e4c9c9f30974e
techSupport <apanasHD2@gmail.com>
2024-12-10 18:23:47 +0300

    тест ветки

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log('true'+true);
+const a = 1;
+const b = 2;
+console.log(a+b)
+console.log(Number(a)+Number(b))
+console.log(String(a)+String(b))
+console.log(Number(undefined))
+console.log(Number(null))
+console.log(Number(true))
+console.log(Number('st'))
+console.log(Number('123'))
+console.log(typeof a)
+console.log(typeof b)
+
+
+let x='12'
+console.log(+x)
+console.log(Number(a)/Number(b))
+console.log(Number(a)*Number(b))
+console.log(String(a)+Number(b))
+console.log(String(a)*Number(b))
+console.log(5%3*Number(a)) 
+console.log(4%2) 
+console.log(String(a)-String(b))
+console.log(2-true)
+
